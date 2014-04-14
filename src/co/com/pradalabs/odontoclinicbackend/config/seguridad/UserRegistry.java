642c5d41838d89e31e66debc25ff819e935e8279
PradaLabs <andres.prada.labs@gmail.com>
2014-04-14 01:34:20 -0500

    Cambio de estructura de los archivos para configurar el proyecto en Maven

--- src/co/com/pradalabs/odontoclinicbackend/config/seguridad/UserRegistry.java
+++
@@ -1,7 +0,0 @@
-package co.com.pradalabs.odontoclinicbackend.config.seguridad;
-
-public interface UserRegistry {
-	  GaeUser findUser(String userId);
-	  void registerUser(GaeUser newUser);
-	  void removeUser(String userId);
-	}
--- src/co/com/pradalabs/odontoclinicbackend/modelo/historiaclinica/AnalisisPeriodontal.java
+++
@@ -1,9 +0,0 @@
-package co.com.pradalabs.odontoclinicbackend.modelo.historiaclinica; 
-
-public class AnalisisPeriodontal {
-
-    public AnalisisPeriodontal () {
-    }
-
-}
-
